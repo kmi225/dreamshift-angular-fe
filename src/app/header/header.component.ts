856231b66cbd53bb07adc12b6ccb3bac3b3570d8
Kasun Ilangaratne <kasunilangaratne@192.168.1.2>
2026-02-09 20:25:39 +0530

    add header text, css adjustments

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,11 +1,39 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  imports: [
+    CommonModule,
+    RouterLink
+  ],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
-
+  public readonly links = [{
+    text: 'Home',
+    route: 'home'
+  },
+  {
+    text: 'Our Services',
+    route: 'services'
+  },
+  {
+    text: 'Our Process',
+    route: 'services'
+  },
+  {
+    text: 'Aussie Job Toolkit',
+    route: 'services'
+  },
+  {
+    text: 'About Us',
+    route: 'services'
+  },
+  {
+    text: 'Blog',
+    route: 'services'
+  }]
 }
