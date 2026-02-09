069b54cbde8707983d8cf9411cb7fbabd25abb16
Kasun Ilangaratne <kasunilangaratne@192.168.1.2>
2026-02-09 23:37:16 +0530

    implement scroll showing transparent header

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,17 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, TemplateRef, inject, HostListener } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterLink } from '@angular/router';
+import { NgbOffcanvas, NgbOffcanvasModule } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-header',
   imports: [
     CommonModule,
-    RouterLink
+    RouterLink,
+    NgbOffcanvasModule
   ],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
+  private offcanvasService = inject(NgbOffcanvas);
+  private lastScrollY = 0;
+  public isHeaderVisible = true;
+
   public readonly links = [{
     text: 'Home',
     route: 'home'
@@ -35,5 +41,30 @@ export class HeaderComponent {
   {
     text: 'Blog',
     route: 'services'
-  }]
+  }];
+
+  @HostListener('window:scroll', ['$event'])
+  onWindowScroll() {
+    const currentScrollY = window.scrollY;
+    
+    // Show header when scrolling up, hide when scrolling down
+    if (currentScrollY < this.lastScrollY) {
+      // Scrolling up
+      this.isHeaderVisible = true;
+    } else if (currentScrollY > this.lastScrollY && currentScrollY > 100) {
+      // Scrolling down (only hide after scrolling past 100px to avoid hiding at top)
+      this.isHeaderVisible = false;
+    }
+    
+    // Always show header at the very top of the page
+    if (currentScrollY < 10) {
+      this.isHeaderVisible = true;
+    }
+    
+    this.lastScrollY = currentScrollY;
+  }
+
+  public openMenu(content: TemplateRef<any>) {
+    this.offcanvasService.open(content, { position: 'end' });
+  }
 }
