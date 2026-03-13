6b2ea0dd22ba2f46bc961a2c0c9d98271f005fa9
Kasun Ilangaratne <k.ilangaratne@gmail.com>
2026-03-13 23:54:21 +0530

    added tracking template excel to online job search page

--- src/app/pages/job-seach/job-seach.component.ts
+++ src/app/pages/job-seach/job-seach.component.ts
@@ -1,11 +1,17 @@
 import { Component } from '@angular/core';
+import { ExcelPreviewComponent } from '../../components/excel-preview/excel-preview.component';
+import { ExcelDownloadsComponent } from '../../components/excel-downloads/excel-downloads.component';
+import { FullWidthBannerComponent } from '../../components/full-width-banner/full-width-banner.component';
 
 @Component({
   selector: 'app-job-seach',
-  imports: [],
+  imports: [ExcelPreviewComponent, ExcelDownloadsComponent, FullWidthBannerComponent],
   templateUrl: './job-seach.component.html',
   styleUrl: './job-seach.component.scss'
 })
 export class JobSeachComponent {
-
+  readonly xlsxUrl = '/Tracking%20Template%20-%20DreamShift.xlsx';
+  readonly csvUrl = '/Tracking%20Template%20-%20DreamShift.csv';
+  readonly sheetsUrl = 'https://docs.google.com/spreadsheets/d/1DzHlsIzzbDRbMMRnybXQPJvY5ZOXvbAvui6OtDkaLqA/edit?usp=sharing';
+  readonly fileName = 'Tracking Template - DreamShift';
 }
