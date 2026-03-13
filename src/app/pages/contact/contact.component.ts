ad7f4878549b55c8954828ce1487ad48887fae56
Kasun Ilangaratne <k.ilangaratne@gmail.com>
2026-03-13 12:14:02 +0530

    extract excel functionlity to reusable component

--- src/app/pages/contact/contact.component.ts
+++ src/app/pages/contact/contact.component.ts
@@ -13,42 +13,4 @@ import { FormsModule } from '@angular/forms';
   styleUrl: './contact.component.scss'
 })
 export class ContactComponent {
-  public readonly contactFormFields = CONTACT_FORM_FIELDS.map(field => ({
-    ...field,
-    value: '',
-  }));
-  public currentStep = 1;
-  public currentStepFieldInputValue = '';
-
-  public nextStep() {
-    this.getCurrentStepField()!.value = this.currentStepFieldInputValue;
-    this.currentStep++;
-    this.currentStepFieldInputValue = this.getCurrentStepField()!.value;
-  }
-
-  public previousStep() {
-    this.getCurrentStepField()!.value = this.currentStepFieldInputValue;
-    this.currentStep--;
-    this.currentStepFieldInputValue = this.getCurrentStepField()!.value;
-  }
-
-  public getCurrentStepField() {
-    return this.contactFormFields.find(field => field.step === this.currentStep);
-  }
-
-  public getCurrentStepFieldId() {
-    return this.getCurrentStepField()?.id;
-  }
-
-  public getCurrentStepFieldName() {
-    return this.getCurrentStepField()?.name;
-  }
-
-  public getCurrentStepFieldPlaceholder() {
-    return this.getCurrentStepField()?.placeholder;
-  }
-
-  public isNextStepDisabled() {
-    return this.currentStepFieldInputValue.length < 3 || this.currentStepFieldInputValue.length > 200;
-  }
 }
